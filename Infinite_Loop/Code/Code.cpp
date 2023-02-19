2da392df234f66e6d3acc75c968824a1edb705e0
FuzzyPickles92 <97342270+FuzzyPickles92@users.noreply.github.com>
2023-02-19 09:59:11 -0600

    Debugged solutions for the homework assignment.

--- Infinite_Loop/Code/Code.cpp
+++ Infinite_Loop/Code/Code.cpp
@@ -1,3 +1,7 @@
+//DeMario Russell 
+//CIS - 251 C++
+//02/17/2023
+//HW - Infinite Loop
 // Lab 1.3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
@@ -6,44 +10,62 @@ using namespace std;
 
 int main()
 {
-	int choice = 0;
+	//variables for the user's input
 
-	string name = "";
-	string age = "";
-	string occup = "";
+	int choice = 0; // variable to store user's menu choice
 
+	string name = ""; // variable to store user's name
+	int age = 0; // variable to store user's age
+	string occup = ""; // variable to store user's occupation
+
+	//Start of Loop
 	while (choice != -1)
 	{
+		// display menu options
 		cout << "-1: Exit\n";
 		cout << "1: Enter Name\n";
 		cout << "2: Enter Age\n";
 		cout << "3: Enter Occupation\n";
+		
+		// retrieve user's choices
 		cin >> choice;
 
-		/*if (choice == -1)
-			break;*/
-
+		// check if input fails (e.g. user enters a non-integer value)
+		if (cin.fail()) {
+			// clear input buffer to prevent infinite loop
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Sorry that choice is not valid!" << endl;
+			continue;
+		}
+		// handle user's menu choice
 		switch (choice)
 		{
 		case 1:
+			//retrieve user's name
 			cout << "What is your Name: ";
 			cin >> name;
 			break;
 		case 2:
+			//retrieve user's age
 			cout << "What is your Age? ";
 			cin >> age;
 			break;
 		case 3:
+			//retrieve user's occupation
 			cout << "What is your Occupation? ";
 			cin >> occup;
 			break;
+		case -1:
+			//exit the program
+			break;
 		default:
 			// Assume Invalid Menu Choice
-			cout << "Sorry that choice is not valid!";
+			cout << "Sorry that choice is not valid!" << endl;
 			break;
 		}
 	}
-
+	//increase the user's age by one
 	age += 1;
 
 	cout << "Thank you for using our application, " << name << " and hope your career in " << occup << " is successful.";
